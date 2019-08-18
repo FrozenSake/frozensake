92d8bc8e590bcd78190432f143b916a1dbb77ac2
J-Olsson <julian.olsson@gmail.com>
2019-08-18 16:46:06 -0400

    two service accounts and associated keys, tf + gcs

--- cicd/service-account.ps1
+++ cicd/service-account.ps1
@@ -13,19 +13,26 @@ gcloud services enable container.googleapis.com `
 
 gcloud components install kubectl
 
+gcloud iam service-accounts create gitlab-terraform `
+    --display-name "GitLab Terraformer"
+gcloud iam service-accounts keys create --iam-account `
+    gitlab-terraform@$env:PROJECT_ID.iam.gserviceaccount.com ./terraform-key.json
+gcloud projects add-iam-policy-binding --role roles/storage.admin $env:PROJECT_ID `
+    --member=serviceAccount:gitlab-terraform@$env:PROJECT_ID.iam.gserviceaccount.com
+gcloud projects add-iam-policy-binding --role roles/compute.admin $env:PROJECT_ID `
+    --member=serviceAccount:gitlab-terraform@$env:PROJECT_ID.iam.gserviceaccount.com
+gcloud projects add-iam-policy-binding --role roles/redis.admin $env:PROJECT_ID `
+    --member=serviceAccount:gitlab-terraform@$env:PROJECT_ID.iam.gserviceaccount.com
+gcloud projects add-iam-policy-binding --role roles/container.admin $env:PROJECT_ID `
+    --member=serviceAccount:gitlab-terraform@$env:PROJECT_ID.iam.gserviceaccount.com
+gcloud projects add-iam-policy-binding --role roles/iam.serviceAccountUser $env:PROJECT_ID `
+    --member=serviceAccount:gitlab-terraform@$env:PROJECT_ID.iam.gserviceaccount.com
+gcloud projects add-iam-policy-binding --role roles/cloudsql.admin $env:PROJECT_ID `
+    --member=serviceAccount:gitlab-terraform@$env:PROJECT_ID.iam.gserviceaccount.com
+
 gcloud iam service-accounts create gitlab-gcs `
     --display-name "GitLab Cloud Storage"
 gcloud iam service-accounts keys create --iam-account `
     gitlab-gcs@$env:PROJECT_ID.iam.gserviceaccount.com ./gcs-key.json
 gcloud projects add-iam-policy-binding --role roles/storage.admin $env:PROJECT_ID `
-    --member=serviceAccount:gitlab-gcs@$env:PROJECT_ID.iam.gserviceaccount.com
-gcloud projects add-iam-policy-binding --role roles/compute.admin $env:PROJECT_ID `
-    --member=serviceAccount:gitlab-gcs@$env:PROJECT_ID.iam.gserviceaccount.com
-gcloud projects add-iam-policy-binding --role roles/redis.admin $env:PROJECT_ID `
-    --member=serviceAccount:gitlab-gcs@$env:PROJECT_ID.iam.gserviceaccount.com
-gcloud projects add-iam-policy-binding --role roles/container.admin $env:PROJECT_ID `
-    --member=serviceAccount:gitlab-gcs@$env:PROJECT_ID.iam.gserviceaccount.com
-gcloud projects add-iam-policy-binding --role roles/iam.serviceAccountUser $env:PROJECT_ID `
-    --member=serviceAccount:gitlab-gcs@$env:PROJECT_ID.iam.gserviceaccount.com
-gcloud projects add-iam-policy-binding --role roles/cloudsql.admin $env:PROJECT_ID `
     --member=serviceAccount:gitlab-gcs@$env:PROJECT_ID.iam.gserviceaccount.com
